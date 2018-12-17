37ef426db3d4ef4778c69e991f9cc20e5a7e63c7
hth_developers <talhamanzoor006@gmail.com>
2018-12-17 11:42:38 +0500

    updated

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import Tilt from 'react-tilt'
+import React from 'react';
+import Tilt from 'react-tilt';
+import '../../main.scss';
 
 const Logo = () =>{
     return(
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt ba2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+        <div className='body ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
             <div className="Tilt-inner"> 👽 </div>
             </Tilt>
         </div>
