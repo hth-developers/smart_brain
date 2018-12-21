efed10debb513391fcd455407d972bd0533bb2cc
hth_developers <talhamanzoor006@gmail.com>
2018-12-22 01:27:24 +0500

    Love it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLink from './components/ImageLink/ImageLink';
+import Rank from './components/Rank/Rank';
 
+const particalsOption = {
+  particles:{
+    number:{
+      value:300,
+      density:{
+        enable: true,
+        value_area:800
+      }
+    }
+  }
+
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particals' 
+                      params={particalsOption}
+                       />
        <Navigation />
        <Logo />
+       <Rank />
        <ImageLink />
        {/*<FaceRecognition />*/}
       </div>
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () =>{
+    return(
+        <div>
+        <div className='white f3 '>
+        {'Talha, Your current rank is....'}
+        </div>
+        <div className='white f1'>
+        {'#4'}
+        </div>
+        </div>
+    );
+}
+export default Rank;
